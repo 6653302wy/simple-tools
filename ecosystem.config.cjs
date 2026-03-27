fbb460798fe0324e3463fd7dfbbc7b39441fa7d0
dev-dot-d <2863234@qq.com>
2026-03-28 00:16:29 +0800

    chore: add pm2 deploy config

---
+++ ecosystem.config.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+    apps: [
+        {
+            name: 'simple-tools',
+            script: './node_modules/next/dist/bin/next',
+            args: 'start -p 3000',
+            cwd: __dirname,
+            exec_mode: 'fork',
+            instances: 1,
+            autorestart: true,
+            watch: false,
+            max_memory_restart: '512M',
+            env: {
+                NODE_ENV: 'production',
+                PORT: '3000',
+            },
+        },
+    ],
+};
